18f37b0aa56c79b699f06790abeacd1810d84da0
Yaskabro <jako169@hotmail.it>
2025-01-07 16:54:21 +0100

    iniziati i nuovi esercizi

--- React/seconda-settimana/2.0.1-Creare-un-contatore-semplice/src/components/HelloClass.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class HelloClass extends React.Component {
-    render(){
-        return <h2>Hello World fatto col metodo turbo feccia!</h2>
-    }
-}
-
-export default HelloClass;
