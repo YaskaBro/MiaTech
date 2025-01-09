64b2659a9dae6e0233cbd52cb1b73c9e7b8be366
Yaskabro <jako169@hotmail.it>
2025-01-09 16:49:42 +0100

    incrementa contatore

--- React/seconda-settimana/2.0.3-Incrementa-Contatore/src/App.jsx
+++ React/seconda-settimana/2.0.3-Incrementa-Contatore/src/App.jsx
@@ -3,11 +3,14 @@ import Counter from "./components/Counter"
 
 const App = () => {
     const [ncounter, setncounter] = useState(0)
+    const increment = () => {
+        setncounter(ncounter + 1)
+    }
 
     return (
         <>
             <div>
-                <button> Aggiungi</button>
+                <button onClick={increment}> Aggiungi</button>
                 <Counter number= {ncounter}/>
             </div>
         </>
