7e2f5142635194684bc3734a4f8d738186215153
Yaskabro <jako169@hotmail.it>
2025-01-17 15:51:57 +0100

    3.0.6 react

---
+++ React/terza-settimana/3.0.6-Memorizza-la-lista-dei-to-do-filtrati-con-useMemo/src/components/TodoList.jsx
@@ -0,0 +1,41 @@
+import React, { useCallback } from 'react'
+import { useFetch } from "../hooks/useFetch";
+import { useFilteredTodos } from "../hooks/useFilteredTodos"
+import { useState } from 'react';
+import { useMemo } from 'react';
+
+const API_URL = "https://jsonplaceholder.typicode.com/todos"
+
+function TodoList() {
+
+    const { data: posts, error, loading, reload } = useFetch(API_URL, { method: "GET" })
+    const [inputValue, setInputValue] = useState('')
+    const filtrati = useMemo(() => {
+        if(!posts) return [];
+        return posts.filter(post =>
+            post.title.toLowerCase().includes(inputValue.toLocaleLowerCase())
+        )
+    })
+      
+    const handleChange = useCallback((event) => {
+        setInputValue(event.target.value)
+    },[]) 
+    
+    return (
+        <div>
+            {loading && <p>Caricamento in corso...</p>}
+            
+            {error && <p>Si è verificato un errore: {error.message}</p>}
+
+            Filtra: <input type="text" value={inputValue} onChange={handleChange}/>
+            <ul>
+                {filtrati && filtrati.map((post) => (
+                    <li key={post.id}>user id: ({post.userId}) id del post: ({post.id}) titolo: "{post.title}"</li>  
+                ))
+            }
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
