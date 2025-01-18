03735ba5f6e99cf53dadb8f753888cc32e5f65cd
Yaskabro <jako169@hotmail.it>
2025-01-18 16:36:52 +0100

    3.0.9

---
+++ React/terza-settimana/3.0.9-Integra-TodoContext-in-TodoList/src/App.jsx
@@ -0,0 +1,14 @@
+import TodoList from "./components/TodoList"
+import { useFetch } from "./hooks/useFetch";
+
+
+const App = () => {
+    
+    return (
+        <>
+            <TodoList />
+        </>
+    )
+}
+
+export default App
