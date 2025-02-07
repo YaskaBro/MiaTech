969ce429b411b56c293587a20d313631e0455b37
Yaskabro <jako169@hotmail.it>
2025-02-07 17:10:09 +0100

    1.1.0

---
+++ Typescript/settimana-1/1.1.0-Gestione-dei-tipi-Dinamici-con-Unknown/todo-list/src/index.ts
@@ -0,0 +1,38 @@
+import { Todo } from "./types";
+
+const todos: Todo[] = []
+
+const addTodo = (title: string) => {
+    return {
+        id: todos.length + 1,
+        title: title,
+        completed: false
+    }
+}
+
+const pippo = addTodo("pippo");
+todos.push(pippo)
+
+const assignTodoToUser = (todo: Todo, userId: number) => {
+    todo.userId = userId
+}
+
+assignTodoToUser(pippo, 123);
+
+const getUserTodos = (userId: number) => todos.filter(t => t.userId === userId);
+
+console.log(getUserTodos(123));
+
+const throwError = (message?: string):never  => {
+    throw new Error (message)
+}
+
+const parseInput = (input: unknown): string => {
+    if (typeof input=== "string") {
+        return input;
+    } else if (typeof input=== "number") {
+        return input.toString()
+    } else {
+        throw new Error (`Input non valido: ${input}`)  as never;
+    }
+}
