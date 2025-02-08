3d1a5636f74c14a4166d071c231d5810ce24c3bb
Yaskabro <jako169@hotmail.it>
2025-02-08 12:30:43 +0100

    1.1.3 typescript

---
+++ Typescript/settimana-1/1.1.3-Utilizzare-Tipi-Union/todo-list/src/index.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var todos = [];
+var addTodo = function (title, metadata) {
+    return {
+        id: todos.length + 1,
+        title: title,
+        completed: false,
+        metadata: metadata
+    };
+};
+var pippo = addTodo("pippo");
+todos.push(pippo);
+var assignTodoToUser = function (todo, userId) {
+    todo.userId = userId;
+};
+assignTodoToUser(pippo, 123);
+var getUserTodos = function (userId) { return todos.filter(function (t) { return t.userId === userId; }); };
+console.log(getUserTodos(123));
+var throwError = function (message) {
+    throw new Error(message);
+};
+var parseInput = function (input) {
+    if (typeof input === "string") {
+        return input;
+    }
+    else if (typeof input === "number") {
+        return input.toString();
+    }
+    else {
+        throw new Error("Input non valido: ".concat(input));
+    }
+};
+throwError("ciao");
+parseInput("ciao");
---
+++ Typescript/settimana-1/1.1.3-Utilizzare-Tipi-Union/todo-list/src/index.ts
@@ -0,0 +1,43 @@
+import { Todo } from "./types";
+
+const todos: Todo[] = []
+
+const addTodo = (title: string, metadata?:string | object) => {
+    return {
+        id: todos.length + 1,
+        title: title,
+        completed: false,
+        metadata: metadata
+    }
+}
+
+const pippo = addTodo("pippo");
+todos.push(pippo)
+
+const assignTodoToUser = (todo: Todo, userId: number) => {
+    todo.userId = userId
+}
+
+assignTodoToUser(pippo, 123);
+
+const getUserTodos = (userId: number) => todos.filter(t => t.userId === userId);
+
+console.log(getUserTodos(123));
+
+const throwError = (message?: string):never  => {
+    throw new Error (message)
+}
+
+const parseInput = (input: unknown): string => {
+    if (typeof input=== "string") {
+        return input;
+    } else if (typeof input=== "number") {
+        return input.toString()
+    } else {
+        throw new Error (`Input non valido: ${input}`) as never;
+    }
+}
+
+throwError("ciao")
+
+parseInput("ciao")
