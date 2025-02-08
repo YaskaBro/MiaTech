8c7441d92e29869d402facde64d6f976e81e4418
Yaskabro <jako169@hotmail.it>
2025-02-08 11:32:53 +0100

    1.1.1 typescript

---
+++ Typescript/settimana-1/1.1.1-Utilizza-il-Tipo-Any/todo-list/src/types.ts
@@ -0,0 +1,13 @@
+export interface Todo {
+    id: number,
+    title: string,
+    completed: boolean,
+    userId?: number,
+    metadata: any
+}
+
+export interface User {
+    id: number,
+    name: string,
+    email? : string
+}
