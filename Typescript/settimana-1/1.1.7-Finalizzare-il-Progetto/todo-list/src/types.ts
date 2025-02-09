6d392e1dad90faa39fcf096dc0ec523f68e39c97
Yaskabro <jako169@hotmail.it>
2025-02-09 14:49:58 +0100

    finiti

---
+++ Typescript/settimana-1/1.1.7-Finalizzare-il-Progetto/todo-list/src/types.ts
@@ -0,0 +1,22 @@
+export interface Todo {
+    id: number,
+    title: string,
+    completed: boolean,
+    userId?: number,
+}
+
+export interface User {
+    id: number,
+    name: string,
+    email?: string
+    readonly todos: Todo[]
+}
+
+export interface TodoWithMetadata extends Todo {
+    metadata: any
+}
+
+export interface Project {
+    users: User[];
+    todos: Todo[];
+}
